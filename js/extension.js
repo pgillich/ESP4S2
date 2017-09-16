d531ea68badde9275f5541a9342d5adeedb668d8
Peter Gillich <pgillich@gmail.com>
2017-09-16 22:17:22 +0100

    Only initNet

---
+++ js/extension.js
@@ -0,0 +1,51 @@
+/*
+ https://raw.githubusercontent.com/LLK/scratchx/master/random_wait_extension.js
+*/
+
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // Functions for block with type 'w' will get a callback function as the 
+    // final argument. This should be called to indicate that the block can
+    // stop waiting.
+    ext.wait_random = function(callback) {
+        wait = Math.random();
+        console.log('Waiting for ' + wait + ' seconds');
+        window.setTimeout(function() {
+            callback();
+        }, wait*1000);
+    };
+
+    function sendRequest(parameter) {
+    	$.get("http://localhost:58266/"+parameter, function(data, textStatus, jqXHR) {
+    		console.log(""+textStatus+": "+data);
+    		return data;
+    	}).fail( function(jqXHR, textStatus, errorThrown) {
+    		console.log(""+textStatus+": "+errorThrown);
+    	});
+    	
+    	return "";
+    }
+    
+    ext.initNet = function(net, maskBits) {
+    	return sendRequest("initNet/"+net+"/"+maskBits);
+    }
+    
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['w', 'wait for random time', 'wait_random'],
+    		[" ", "Set network %s / %n", "initNet", "192.168.10.0", "24"],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('ESP for Scratch 2', descriptor, ext);
+})({});
